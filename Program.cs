6a124e16f97d5376c0179b7193ef36e24b263f79
Kate <kate@gmail.com>
2023-02-05 19:26:18 +0300

    Решена Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.

--- Program.cs
+++ Program.cs
@@ -137,7 +137,7 @@ Console.WriteLine(result);
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
-
+/*
 double LineLength (double x1, double x2, double y1, double y2, double z1, double z2)
 {
     double LineLength1, LineLength2, LineLength3, result;
@@ -163,3 +163,21 @@ Console.Write("Введите координату Z второй точки: ")
 double z2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Длина отрезка: " + Math.Round(LineLength(x1, y1, z1, x2, y2, z2) , 2));
+*/
+
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void Cubes(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        Console.Write(i * i * i + "  ");
+    }
+}
+Console.Write("Input a number:  ");
+Cubes(Convert.ToInt32(Console.ReadLine()));
+
